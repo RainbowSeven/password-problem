3b9532204a3ea0ef61b60fc5c7535ca5a8e5bfac
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-12 19:38:13 +0100

    Fix write to .output

--- solution.py
+++ solution.py
@@ -4,10 +4,11 @@ def check(a, b, p):
     """Check for average number of expected keystrokes"""
     f = []
     f.append(0.0)
-    enter = 2 + b
     
     for i in range(1, a+1):
         f.append((1 - p[i-1]) + p[i-1] * f[i-1])
+    
+    enter = 2 + b
     finish =  (b - a) + 1 + f[a]*(1 + b) 
     backsp = finish
     for back in range(1, a+1): 
@@ -15,33 +16,45 @@ def check(a, b, p):
     
     return min(enter, backsp)
     
+def writeresult(out):
+    """ Write results to file """
+    with open(".output","w") as results:
+        results.write(out)
+
+def getparam(inp, i):
+    """ Get params form .input file """
+    
+    with open(inp) as paramf:
+        p = paramf.readlines()
+        return p[i]
 
 if __name__ == '__main__':
     if len(sys.argv[1]) < 2:
         print "Please specify an input file to solve"
     else:
-        with open(sys.argv[1], 'r') as my_file:
-            lines = sum(1 for line in my_file)
-            keystrk_passlen = [int(i) for i in range(1, lines) if i % 2 == 1 ]
-            with open(sys.argv[1], 'r') as my_file:
-                file_rd = my_file.readlines()
-                case = 1
-                out = ''
-                # clock duration of execution
-                start = time.clock()
-                for i in keystrk_passlen:
-                    keystrk_strls = (file_rd[i]).replace('\n','').split(' ')
-                    key_strk_fls = [int(j) for j in keystrk_strls]
+        fname = sys.argv[1]
+        with open(fname, 'r') as params:
+            frd = params.readlines()
+            tc = int(frd[0])
+        report_buf = ''
+        # Clock duration of execution
+        start = time.clock()
+        odd = [ i for i in range(1,2*tc +1) if  i % 2 != 0]
+        even = [ i for i in range(1,2*tc +1) if i % 2 == 0]
 
-                    prob_strls = (file_rd[i+1]).replace('\n','').split(' ')
-                    prob_fls =  [ float(k) for k in  prob_strls]
-                    # Validate parameters
-                    if filter(lambda x: x, [i for i in key_strk_fls if i > 1 or i<0 ]) :
-                        print "[!] Skipping invalid parameters"
-                        continue
-                    add_keystrk = check(key_strk_fls[0],key_strk_fls[1], prob_fls)
-                    out +=  "Case # %s: %s \n" %( str(case), str(add_keystrk))
-                    case +=1
-                print out
-                end = time.clock()
-                print "This solution ran in %s seconds" %(str(end-start))
+        for x in range(0, tc):
+            prob_strls = (getparam(fname, even[x])).replace('\n','').split(' ')
+            
+            ksslen = (getparam(fname, odd[x])).replace('\n','').split(' ')
+
+            ksfls = [int(j) for j in ksslen]
+            prob_fls =  [ float(k) for k in  prob_strls]
+            # Run check
+            
+            add_keystrk = check(ksfls[0], ksfls[1], prob_fls)
+            report = "Case # %s: %s \n" %( str(x+1), str(add_keystrk))
+            report_buf += report
+        end = time.clock()
+        writeresult(report_buf)
+        print report_buf
+        print "This solution ran in %s seconds" %(str(end-start))
