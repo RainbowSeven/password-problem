26240fb0cbc5cd4f1ba49fe64225a69f328a6721
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-12 04:56:27 +0100

    Update check variables

--- solution.py
+++ solution.py
@@ -1,20 +1,20 @@
 import sys
 import time
 def check(a, b, p):
-    dp = []
-    dp.append(0.0)
+    f = []
+    f.append(0.0)
     for i in range(1, a+1):
-        dp.append((1 - p[i-1]) + p[i-1] * dp[i-1])
+        f.append((1 - p[i-1]) + p[i-1] * f[i-1])
     
-    giveup = 2 + b
+    enter = 2 + b
     
-    finish = (b - a) + 1 + dp[a]*(1 + b)
+    finish = (b - a) + 1 + f[a]*(1 + b)
     backspace = finish;
 
     for back in range(1, a+1): 
-        backspace = min(backspace, back + (b - a + back) + 1 + dp[a-back]*(1 + b) )
+        backspace = min(backspace, back + (b - a + back) + 1 + f[a-back]*(1 + b) )
     
-    return min(giveup, backspace)
+    return min(enter, backspace)
     
 
 if __name__ == '__main__':
